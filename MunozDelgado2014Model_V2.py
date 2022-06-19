adc5b9b82a5d6be22d1a4d6b8fc1c700afb40cb5
Jonas Villela <jonas.villela@gmail.com>
2022-06-19 12:01:06 -0300

    Update MunozDelgado2014Model_V2.py

--- MunozDelgado2014Model_V2.py
+++ MunozDelgado2014Model_V2.py
@@ -442,15 +442,29 @@ model.delta_l_srktbv = pyo.Var(model.delta_l_rule,
 #     )
 # =============================================================================
 
+# =============================================================================
+# def f_l_rule(m):
+#     index = []
+#     for l in model.L:
+#         for s in model.Omega_N:
+#             for r in model.Omega_l_s[l,s]:
+#                 for k in model.K_l[l]:
+#                     for t in model.T:
+#                         for b in model.B:
+#                             index.append((l,s,r,k,t,b))
+#     return index
+#             
+# model.f_l_rule = pyo.Set(dimen=6, initialize=f_l_rule)
+# =============================================================================
 def f_l_rule(m):
     index = []
     for l in model.L:
-        for s in model.Omega_N:
-            for r in model.Omega_l_s[l,s]:
-                for k in model.K_l[l]:
-                    for t in model.T:
-                        for b in model.B:
-                            index.append((l,s,r,k,t,b))
+        for s,r in model.Upsilon_l[l]:
+            for k in model.K_l[l]:
+                for t in model.T:
+                    for b in model.B:
+                        index.append((l,s,r,k,t,b))
+                        index.append((l,r,s,k,t,b))
     return index
             
 model.f_l_rule = pyo.Set(dimen=6, initialize=f_l_rule)
